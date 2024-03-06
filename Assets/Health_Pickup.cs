7ee0de015c46cd241af0de698ef030e22c18b7d1
LilGenius03 <finnarcher03@gmail.com>
2024-03-06 16:54:32 +0000

    changes

--- Assets/Health_Pickup.cs
+++ Assets/Health_Pickup.cs
@@ -6,24 +6,17 @@ public class Health_Pickup : MonoBehaviour
 {
     [SerializeField] private float healthIncrease;
     private PlayerHealth playerHealth;
-    public GameObject Player;
     // Start is called before the first frame update
     private void Awake()
     {
-        playerHealth = Player.GetComponent<PlayerHealth>();
+        playerHealth = GameManager.instance.GetComponent<PlayerHealth>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player") && playerHealth.currentHealth > 0f)
         {
-
             if(playerHealth.currentHealth < playerHealth.maxHealth) 
             {
                 playerHealth.currentHealth += healthIncrease;
